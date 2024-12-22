caa787105b13b93c86f90452f2ae4733b6dd982d
AmmarMatarieh2000 <ammarmatarieh2@gmail.com>
2024-12-22 20:47:50 +0300

    Upload Pub dev

--- lib/src/colors_materiel.dart
+++ lib/src/colors_materiel.dart
@@ -33,134 +33,519 @@ class MaterialColors {
 
   /// A light shade of lavenderBlush, resembling lavenderBlush.
   static const Color lavenderBlush = Color(0xFFFFF0F5);
+
+  /// A light shade of papayaWhip, resembling papayaWhip.
   static const Color papayaWhip = Color(0xFFFFEFD5);
+
+  /// A light shade of blanchedAlmond, resembling blanchedAlmond.
   static const Color blanchedAlmond = Color(0xFFFFEBCD);
+
+  /// A light shade of mistyRose, resembling mistyRose.
+
   static const Color mistyRose = Color(0xFFFFE4E1);
+
+  /// A light shade of bisque, resembling bisque.
   static const Color bisque = Color(0xFFFFE4C4);
+
+  /// A light shade of moccasin, resembling moccasin.
   static const Color moccasin = Color(0xFFFFE4B5);
+
+  /// A light shade of navajoWhite, resembling navajoWhite.
   static const Color navajoWhite = Color(0xFFFFDEAD);
+
+  /// A light shade of peachPuff, resembling peachPuff.
+
   static const Color peachPuff = Color(0xFFFFDAB9);
+
+  /// A light shade of gold, resembling gold.
+
   static const Color gold = Color(0xFFFFD700);
+
+  /// A light shade of pink, resembling pink.
+
   static const Color pink = Color(0xFFFFC0CB);
+
+  /// A light shade of lightPink, resembling lightPink.
+
   static const Color lightPink = Color(0xFFFFB6C1);
+
+  /// A light shade of orange, resembling orange.
+
   static const Color orange = Color(0xFFFFA500);
+
+  /// A light shade of lightSalmon, resembling lightSalmon.
+
   static const Color lightSalmon = Color(0xFFFFA07A);
+
+  /// A light shade of darkOrange, resembling darkOrange.
+
   static const Color darkOrange = Color(0xFFFF8C00);
+
+  /// A light shade of coral, resembling coral.
+
   static const Color coral = Color(0xFFFF7F50);
+
+  /// A light shade of hotPink, resembling hotPink.
+
   static const Color hotPink = Color(0xFFFF69B4);
+
+  /// A light shade of tomato, resembling tomato.
+
   static const Color tomato = Color(0xFFFF6347);
+
+  /// A light shade of orangeRed, resembling orangeRed.
+
   static const Color orangeRed = Color(0xFFFF4500);
+
+  /// A light shade of deepPink, resembling deepPink.
+
   static const Color deepPink = Color(0xFFFF1493);
+
+  /// A light shade of fuchsia, resembling fuchsia.
+
   static const Color fuchsia = Color(0xFFFF00FF);
+
+  /// A light shade of magenta, resembling magenta.
+
   static const Color magenta = Color(0xFFFF00FF);
+
+  /// A light shade of red, resembling red.
+
   static const Color red = Color(0xFFFF0000);
+
+  /// A light shade of oldLace, resembling oldLace.
+
   static const Color oldLace = Color(0xFFFDF5E6);
+
+  /// A light shade of lightGoldenrodYellow, resembling lightGoldenrodYellow.
+
   static const Color lightGoldenrodYellow = Color(0xFFFAFAD2);
+
+  /// A light shade of linen, resembling linen.
+
   static const Color linen = Color(0xFFFAF0E6);
+
+  /// A light shade of antiqueWhite, resembling antiqueWhite.
+
   static const Color antiqueWhite = Color(0xFFFAEBD7);
+
+  /// A light shade of salmon, resembling salmon.
+
   static const Color salmon = Color(0xFFFA8072);
+
+  /// A light shade of ghostWhite, resembling ghostWhite.
+
   static const Color ghostWhite = Color(0xFFF8F8FF);
+
+  /// A light shade of mintCream, resembling mintCream.
+
   static const Color mintCream = Color(0xFFF5FFFA);
+
+  /// A light shade of whiteSmoke, resembling whiteSmoke.
+
   static const Color whiteSmoke = Color(0xFFF5F5F5);
+
+  /// A light shade of beige, resembling beige.
+
   static const Color beige = Color(0xFFF5F5DC);
+
+  /// A light shade of wheat, resembling wheat.
+
   static const Color wheat = Color(0xFFF5DEB3);
+
+  /// A light shade of sandyBrown, resembling sandyBrown.
+
   static const Color sandyBrown = Color(0xFFF4A460);
+
+  /// A light shade of azure, resembling azure.
+
   static const Color azure = Color(0xFFF0FFFF);
+
+  /// A light shade of honeydew, resembling honeydew.
+
   static const Color honeydew = Color(0xFFF0FFF0);
+
+  /// A light shade of aliceBlue, resembling aliceBlue.
+
   static const Color aliceBlue = Color(0xFFF0F8FF);
+
+  /// A light shade of khaki, resembling khaki.
+
   static const Color khaki = Color(0xFFF0E68C);
+
+  /// A light shade of lightCoral, resembling lightCoral.
+
   static const Color lightCoral = Color(0xFFF08080);
+
+  /// A light shade of paleGoldenrod, resembling paleGoldenrod.
+
   static const Color paleGoldenrod = Color(0xFFEEE8AA);
+
+  /// A light shade of violet, resembling violet.
+
   static const Color violet = Color(0xFFEE82EE);
+
+  /// A light shade of darkSalmon, resembling darkSalmon.
+
   static const Color darkSalmon = Color(0xFFE9967A);
+
+  /// A light shade of lavender, resembling lavender.
+
   static const Color lavender = Color(0xFFE6E6FA);
+
+  /// A light shade of lightCyan, resembling lightCyan.
+
   static const Color lightCyan = Color(0xFFE0FFFF);
+
+  /// A light shade of burlyWood, resembling burlyWood.
+
   static const Color burlyWood = Color(0xFFDEB887);
+
+  /// A light shade of plum, resembling plum.
+
   static const Color plum = Color(0xFFDDA0DD);
+
+  /// A light shade of gainSboro, resembling gainSboro.
+
   static const Color gainSboro = Color(0xFFDCDCDC);
+
+  /// A light shade of crimson, resembling crimson.
+
   static const Color crimson = Color(0xFFDC143C);
+
+  /// A light shade of paleVioletRed, resembling paleVioletRed.
+
   static const Color paleVioletRed = Color(0xFFDB7093);
+
+  /// A light shade of goldenrod, resembling goldenrod.
+
   static const Color goldenrod = Color(0xFFDAA520);
+
+  /// A light shade of orchid, resembling orchid.
+
   static const Color orchid = Color(0xFFDA70D6);
+
+  /// A light shade of thistle, resembling thistle.
+
   static const Color thistle = Color(0xFFD8BFD8);
+
+  /// A light shade of lightGrey, resembling lightGrey.
+
   static const Color lightGrey = Color(0xFFD3D3D3);
+
+  /// A light shade of tan, resembling tan.
+
   static const Color tan = Color(0xFFD2B48C);
+
+  /// A light shade of chocolate, resembling chocolate.
+
   static const Color chocolate = Color(0xFFD2691E);
+
+  /// A light shade of peru, resembling peru.
+
   static const Color peru = Color(0xFFCD853F);
+
+  /// A light shade of indianRed, resembling indianRed.
+
   static const Color indianRed = Color(0xFFCD5C5C);
+
+  /// A light shade of mediumVioletRed, resembling mediumVioletRed.
+
   static const Color mediumVioletRed = Color(0xFFC71585);
+
+  /// A light shade of silver, resembling silver.
+
   static const Color silver = Color(0xFFC0C0C0);
+
+  /// A light shade of darkKhaki, resembling darkKhaki.
+
   static const Color darkKhaki = Color(0xFFBDB76B);
+
+  /// A light shade of rosyBrown, resembling rosyBrown.
+
   static const Color rosyBrown = Color(0xFFBC8F8F);
+
+  /// A light shade of mediumOrchid, resembling mediumOrchid.
+
   static const Color mediumOrchid = Color(0xFFBA55D3);
+
+  /// A light shade of darkGoldenrod, resembling darkGoldenrod.
+
   static const Color darkGoldenrod = Color(0xFFB8860B);
+
+  /// A light shade of fireBrick, resembling fireBrick.
+
   static const Color fireBrick = Color(0xFFB22222);
+
+  /// A light shade of powderBlue, resembling powderBlue.
+
   static const Color powderBlue = Color(0xFFB0E0E6);
+
+  /// A light shade of lightSteelBlue, resembling lightSteelBlue.
+
   static const Color lightSteelBlue = Color(0xFFB0C4DE);
+
+  /// A light shade of paleTurquoise, resembling paleTurquoise.
+
   static const Color paleTurquoise = Color(0xFFAFEEEE);
+
+  /// A light shade of greenYellow, resembling greenYellow.
+
   static const Color greenYellow = Color(0xFFADFF2F);
+
+  /// A light shade of lightBlue, resembling lightBlue.
+
   static const Color lightBlue = Color(0xFFADD8E6);
+
+  /// A light shade of darkGray, resembling darkGray.
+
   static const Color darkGray = Color(0xFFA9A9A9);
+
+  /// A light shade of brown, resembling brown.
+
   static const Color brown = Color(0xFFA52A2A);
+
+  /// A light shade of sienna, resembling sienna.
+
   static const Color sienna = Color(0xFFA0522D);
+
+  /// A light shade of yellowGreen, resembling yellowGreen.
+
   static const Color yellowGreen = Color(0xFF9ACD32);
+
+  /// A light shade of darkOrchid, resembling darkOrchid.
+
   static const Color darkOrchid = Color(0xFF9932CC);
+
+  /// A light shade of paleGreen, resembling paleGreen.
+
   static const Color paleGreen = Color(0xFF98FB98);
+
+  /// A light shade of darkViolet, resembling darkViolet.
+
   static const Color darkViolet = Color(0xFF9400D3);
+
+  /// A light shade of mediumPurple, resembling mediumPurple.
+
   static const Color mediumPurple = Color(0xFF9370DB);
+
+  /// A light shade of lightGreen, resembling lightGreen.
+
   static const Color lightGreen = Color(0xFF90EE90);
+
+  /// A light shade of darkSeaGreen, resembling darkSeaGreen.
+
   static const Color darkSeaGreen = Color(0xFF8FBC8F);
+
+  /// A light shade of saddleBrown, resembling saddleBrown.
+
   static const Color saddleBrown = Color(0xFF8B4513);
+
+  /// A light shade of darkMagenta, resembling darkMagenta.
+
   static const Color darkMagenta = Color(0xFF8B008B);
+
+  /// A light shade of darkRed, resembling darkRed.
+
   static const Color darkRed = Color(0xFF8B0000);
+
+  /// A light shade of blueViolet, resembling blueViolet.
+
   static const Color blueViolet = Color(0xFF8A2BE2);
+
+  /// A light shade of lightSkyBlue, resembling lightSkyBlue.
+
   static const Color lightSkyBlue = Color(0xFF87CEFA);
+
+  /// A light shade of skyBlue, resembling skyBlue.
+
   static const Color skyBlue = Color(0xFF87CEEB);
+
+  /// A light shade of gray, resembling gray.
+
   static const Color gray = Color(0xFF808080);
+
+  /// A light shade of olive, resembling olive.
+
   static const Color olive = Color(0xFF808000);
+
+  /// A light shade of purple, resembling purple.
+
   static const Color purple = Color(0xFF800080);
+
+  /// A light shade of maroon, resembling maroon.
+
   static const Color maroon = Color(0xFF800000);
+
+  /// A light shade of aquamarine, resembling aquamarine.
+
   static const Color aquamarine = Color(0xFF7FFFD4);
+
+  /// A light shade of chartreuse, resembling chartreuse.
+
   static const Color chartreuse = Color(0xFF7FFF00);
+
+  /// A light shade of lawnGreen, resembling lawnGreen.
+
   static const Color lawnGreen = Color(0xFF7CFC00);
+
+  /// A light shade of mediumSlateBlue, resembling mediumSlateBlue.
+
   static const Color mediumSlateBlue = Color(0xFF7B68EE);
+
+  /// A light shade of lightSlateGray, resembling lightSlateGray.
+
   static const Color lightSlateGray = Color(0xFF778899);
+
+  /// A light shade of slateGray, resembling slateGray.
+
   static const Color slateGray = Color(0xFF708090);
+
+  /// A light shade of oliveDrab, resembling oliveDrab.
+
   static const Color oliveDrab = Color(0xFF6B8E23);
+
+  /// A light shade of slateBlue, resembling slateBlue.
+
   static const Color slateBlue = Color(0xFF6A5ACD);
+
+  /// A light shade of dimGray, resembling dimGray.
+
   static const Color dimGray = Color(0xFF696969);
+
+  /// A light shade of mediumAquamarine, resembling mediumAquamarine.
+
   static const Color mediumAquamarine = Color(0xFF66CDAA);
+
+  /// A light shade of cornflowerBlue, resembling cornflowerBlue.
+
   static const Color cornflowerBlue = Color(0xFF6495ED);
+
+  /// A light shade of cadetBlue, resembling cadetBlue.
+
   static const Color cadetBlue = Color(0xFF5F9EA0);
+
+  /// A light shade of darkOliveGreen, resembling darkOliveGreen.
+
   static const Color darkOliveGreen = Color(0xFF556B2F);
+
+  /// A light shade of indigo, resembling indigo.
+
   static const Color indigo = Color(0xFF4B0082);
+
+  /// A light shade of mediumTurquoise, resembling mediumTurquoise.
+
   static const Color mediumTurquoise = Color(0xFF48D1CC);
+
+  /// A light shade of darkSlateBlue, resembling darkSlateBlue.
+
   static const Color darkSlateBlue = Color(0xFF483D8B);
+
+  /// A light shade of steelBlue, resembling steelBlue.
+
   static const Color steelBlue = Color(0xFF4682B4);
+
+  /// A light shade of royalBlue, resembling royalBlue.
+
   static const Color royalBlue = Color(0xFF4169E1);
+
+  /// A light shade of turquoise, resembling turquoise.
+
   static const Color turquoise = Color(0xFF40E0D0);
+
+  /// A light shade of mediumSeaGreen, resembling mediumSeaGreen.
+
   static const Color mediumSeaGreen = Color(0xFF3CB371);
+
+  /// A light shade of limeGreen, resembling limeGreen.
+
   static const Color limeGreen = Color(0xFF32CD32);
+
+  /// A light shade of darkSlateBlue, resembling darkSlateBlue.
+
   static const Color darkSlateGray = Color(0xFF2F4F4F);
+
+  /// A light shade of seaGreen, resembling seaGreen.
+
   static const Color seaGreen = Color(0xFF2E8B57);
+
+  /// A light shade of forestGreen, resembling forestGreen.
+
   static const Color forestGreen = Color(0xFF228B22);
+
+  /// A light shade of lightSeaGreen, resembling lightSeaGreen.
+
   static const Color lightSeaGreen = Color(0xFF20B2AA);
+
+  /// A light shade of dodgerBlue, resembling dodgerBlue.
+
   static const Color dodgerBlue = Color(0xFF1E90FF);
+
+  /// A light shade of midnightBlue, resembling midnightBlue.
+
   static const Color midnightBlue = Color(0xFF191970);
+
+  /// A light shade of aqua, resembling aqua.
+
   static const Color aqua = Color(0xFF00FFFF);
+
+  /// A light shade of cyan, resembling cyan.
+
   static const Color cyan = Color(0xFF00FFFF);
+
+  /// A light shade of springGreen, resembling springGreen.
+
   static const Color springGreen = Color(0xFF00FF7F);
+
+  /// A light shade of lime, resembling lime.
+
   static const Color lime = Color(0xFF00FF00);
+
+  /// A light shade of mediumSpringGreen, resembling mediumSpringGreen.
+
   static const Color mediumSpringGreen = Color(0xFF00FA9A);
+
+  /// A light shade of darkTurquoise, resembling darkTurquoise.
+
   static const Color darkTurquoise = Color(0xFF00CED1);
+
+  /// A light shade of deepSkyBlue, resembling deepSkyBlue.
+
   static const Color deepSkyBlue = Color(0xFF00BFFF);
+
+  /// A light shade of darkCyan, resembling darkCyan.
+
   static const Color darkCyan = Color(0xFF008B8B);
+
+  /// A light shade of teal, resembling teal.
+
   static const Color teal = Color(0xFF008080);
+
+  /// A light shade of green, resembling green.
+
   static const Color green = Color(0xFF008000);
+
+  /// A light shade of darkGreen, resembling darkGreen.
+
   static const Color darkGreen = Color(0xFF006400);
+
+  /// A light shade of blue, resembling blue.
+
   static const Color blue = Color(0xFF0000FF);
+
+  /// A light shade of mediumBlue, resembling mediumBlue.
+
   static const Color mediumBlue = Color(0xFF0000CD);
+
+  /// A light shade of darkBlue, resembling darkBlue.
+
   static const Color darkBlue = Color(0xFF00008B);
+
+  /// A light shade of navy, resembling navy.
+
   static const Color navy = Color(0xFF000080);
+
+  /// A light shade of black, resembling black.
+
   static const Color black = Color(0xFF000000);
 }
